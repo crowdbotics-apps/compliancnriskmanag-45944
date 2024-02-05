6b8792b61bdd68b571f4e6d4d2443c95624c8bef
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:37:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "compliancnriskmanag-45944",
+  url: "https://compliancnriskmanag-45944.botics.co",
+  api: "https://compliancnriskmanag-45944.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
